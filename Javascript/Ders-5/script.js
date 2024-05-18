96cca0337f94ae13afbba38b3a3efa24fa27b558
Şevval İclal Cebecioğlu <161520054+sevvaliclalcebecioglu@users.noreply.github.com>
2024-05-18 23:40:39 +0300

    ders-5

---
+++ Javascript/Ders-5/script.js
@@ -0,0 +1,19 @@
+var name = "İclal Cebecioğlu";
+
+        
+function x(){
+   console.log(name);
+}
+
+function sum(number1, number2){
+   console.log(number1 + number2);
+}
+
+function writeText(text){
+   console.log("writeText metodu çalıştı.")
+   document.write(text);
+}
+
+function bodyOnLoad() {
+   console.log("Body yüklenmesi tamamlandı.");
+}
